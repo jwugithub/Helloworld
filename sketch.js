0975c81d2fd6c90216e997fc03e456fcf6ec540c
jwugithub <jwu1020@yahoo.com>
2025-02-22 15:43:11 +0800

    trae test 1

---
+++ sketch.js
@@ -0,0 +1,34 @@
+let angle = 0;
+let slider;
+
+function setup() {
+    createCanvas(800, 600);
+    slider = createSlider(0, PI, PI/4, 0.01);
+    slider.position(10, 10);
+    angleMode(RADIANS);
+}
+
+function draw() {
+    background(220);
+    angle = slider.value();
+    stroke(255, 0, 0);
+    translate(width/2, height);
+    branch(120);
+}
+
+function branch(len) {
+    line(0, 0, 0, -len);
+    translate(0, -len);
+    
+    if (len > 4) {
+        push();
+        rotate(angle);
+        branch(len * 0.67);
+        pop();
+        
+        push();
+        rotate(-angle);
+        branch(len * 0.67);
+        pop();
+    }
+}
